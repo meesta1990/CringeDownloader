ad56a780a6328e329baada36dec4dd5ffd6732ee
Meesta <marco.mamone9@gmail.com>
2024-07-21 19:43:16 +0300

    various fix

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,24 @@
 const express = require('express');
-const ytdl = require('@distube/ytdl-core');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const tmp = require('tmp');
 const path = require('path');
 const fs = require('fs');
 const axios = require('axios');
-const ffmpeg = require('fluent-ffmpeg');
-const ffmpegPath = require('ffmpeg-static');
-const { alldl } = require('rahad-all-downloader');
+const youtubedl = require('youtube-dl-exec')
+const https = require('https');
+const getFBInfo  = require('@xaviabot/fb-downloader');
+
+
+const Tiktok = require("@tobyg74/tiktok-api-dl")
+const instagramGetUrl = require('instagram-url-direct');
+const tt = require('twitter-dl');
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
-ffmpeg.setFfmpegPath(ffmpegPath);
 
 const isYoutubeUrl = (url) => {
   const regex = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/;
@@ -32,6 +35,21 @@ const isRedditUrl = (url) => {
   return regex.test(url);
 };
 
+const isTikTokUrl = (url) => {
+  const regex = /^.*https:\/\/(?:m|www|vm)?\.?tiktok\.com\/((?:.*\b(?:(?:usr|v|embed|user|video)\/|\?shareId=|\&item_id=)(\d+))|\w+)/;
+  return regex.test(url);
+};
+
+const isFacebookUrl = (url) => {
+  const regex = /^(https?:\/\/)?(www\.)?facebook\.com\/.+$/;
+  return regex.test(url);
+};
+
+const isInstagramUrl = (url) => {
+  const regex = /^(https?:\/\/)?(www\.)?instagram\.com\/.+$/;
+  return regex.test(url);
+};
+
 const getRedditVideoUrl = async (url) => {
   try {
     const response = await axios.get(`${url}.json`);
@@ -45,6 +63,46 @@ const getRedditVideoUrl = async (url) => {
   }
 };
 
+const getFacebookVideoUrl = async (url) => {
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+      }
+    });
+
+    const videoUrlMatch = response.data.match(/"hd_src":"(https:[^"]+)"/);
+    if (videoUrlMatch && videoUrlMatch[1]) {
+      return videoUrlMatch[1].replace(/\\u0025/g, '%').replace(/\\/g, '');
+    } else {
+      const videoUrlMatchSD = response.data.match(/"sd_src":"(https:[^"]+)"/);
+      if (videoUrlMatchSD && videoUrlMatchSD[1]) {
+        return videoUrlMatchSD[1].replace(/\\u0025/g, '%').replace(/\\/g, '');
+      }
+    }
+    return null;
+  } catch (error) {
+    console.error('Error fetching Facebook video URL:', error);
+    return null;
+  }
+};
+
+const getLongUrl = async (url) => {
+  try {
+    const response = await axios.get(url, {
+      maxRedirects: 0,
+      validateStatus: (status) => status === 302 || status === 301
+    });
+    return response.headers.location;
+  } catch (error) {
+    if (error.response && error.response.status === 302) {
+      return error.response.headers.location;
+    }
+    console.error('Error fetching long  URL:', error);
+    return null;
+  }
+};
+
 app.post('/api/download', async (req, res) => {
   let { url } = req.body;
   if (!url) {
@@ -53,80 +111,42 @@ app.post('/api/download', async (req, res) => {
 
   if (isYoutubeUrl(url)) {
     try {
-      // Create temporary files for video and audio
-      const videoTmp = tmp.fileSync();
-      const audioTmp = tmp.fileSync();
-      const outputTmp = tmp.fileSync({ postfix: '.mp4' });
-
-      // Get video info
-      const info = await ytdl.getInfo(url);
-      const title = info.videoDetails.title.replace(/[^a-zA-Z0-9]/g, '_'); // Sanitize title
-
-      // Download video and audio streams
-      const videoStream = ytdl(url, { filter: 'videoonly' }).pipe(fs.createWriteStream(videoTmp.name));
-
-      videoStream.on('finish', () => {
-        console.log('Video download finished');
-        const audioStream = ytdl(url, { filter: 'audioonly' }).pipe(fs.createWriteStream(audioTmp.name));
-
-        audioStream.on('finish', () => {
-          console.log('Audio download finished');
-
-          // Combine video and audio using ffmpeg
-          ffmpeg()
-            .input(videoTmp.name)
-            .input(audioTmp.name)
-            .outputOptions('-c:v copy')
-            .outputOptions('-c:a aac')
-            .save(outputTmp.name)
-            .on('end', () => {
-              console.log('Combining video and audio finished');
-
-              // Send the file for download
-              res.download(outputTmp.name, `${title}.mp4`, (err) => {
-                if (err) {
-                  console.error('Error sending file:', err);
-                  res.status(500).json({ error: 'Error sending file' });
-                } else {
-                  console.log('Download sent successfully');
-                  // Clean up temporary files
-                  videoTmp.removeCallback();
-                  audioTmp.removeCallback();
-                  outputTmp.removeCallback();
-                }
-              });
-            })
-            .on('error', (err) => {
-              console.error('Error combining video and audio:', err);
-              res.status(500).json({ error: 'Error combining video and audio' });
-              // Clean up temporary files in case of error
+      youtubedl(url, {
+        dumpSingleJson: true,
+        noCheckCertificates: true,
+        noWarnings: true,
+        preferFreeFormats: true,
+        addHeader: ['referer:youtube.com', 'user-agent:googlebot']
+      }).then(async (output) => {
+        const videoTmp = tmp.fileSync({ postfix: '.mp4' });
+        const response = await axios({
+          url: output.url,
+          method: 'GET',
+          responseType: 'stream',
+        })
+        response.data.pipe(fs.createWriteStream(videoTmp.name))
+        .on('finish', () => {
+          console.log('Video downloaded successfully');
+          res.download(videoTmp.name, `youtube_video.mp4`, (err) => {
+            if (err) {
+              console.error('Error sending file:', err);
+              res.status(500).json({ error: 'Error sending file' });
+            } else {
               videoTmp.removeCallback();
-              audioTmp.removeCallback();
-              outputTmp.removeCallback();
-            });
-        });
-
-        audioStream.on('error', (err) => {
-          console.error('Error downloading audio:', err);
-          res.status(500).json({ error: 'Error downloading audio' });
+            }
+          });
+        })
+        .on('error', (err) => {
+          console.error('Error downloading video:', err);
+          res.status(500).json({ error: 'Error downloading video' });
           // Clean up temporary files in case of error
           videoTmp.removeCallback();
-          audioTmp.removeCallback();
-
-          videoStream.on('error', (err) => {
-            console.error('Error downloading video:', err);
-            res.status(500).json({ error: 'Error downloading video' });
-            // Clean up temporary files in case of error
-            videoTmp.removeCallback();
-          });
         })
       })
     } catch (error) {
       console.error('Error downloading video:', error);
       res.status(500).json({ error: 'Error downloading video' });
     }
-  } else if (isTwitterUrl(url)) {
-    res.status(500).json({ error: 'Not supported yet' });
   } else if (isRedditUrl(url)) {
     try {
       const videoUrl = await getRedditVideoUrl(url);
@@ -164,10 +184,28 @@ app.post('/api/download', async (req, res) => {
       console.error('Error:', error.message);
       res.status(500).json({ error: 'Error downloading video' });
     }
-  } else {
+  } else{
     try {
-      const result = await alldl(url);
-      const videoUrl = result.data.videoUrl;
+      let videoUrl;
+
+      if (isTikTokUrl(url)) {
+        const longUrl = await getLongUrl(url)
+        const result = await Tiktok.Downloader(longUrl, { version: "v1" })
+        videoUrl = result.result.video.downloadAddr
+      } else if (isFacebookUrl(url)) {
+        videoUrl = await getFBInfo(url)
+        .then((result) => result.hd || result.sd)
+        .catch((error) => {
+
+        });        
+      } else if (isInstagramUrl(url)) {
+        videoUrl = await instagramGetUrl(url).then(result => result.url_list[0]);
+      } else if (isTwitterUrl(url)) {
+        return res.status(400).json({ error: 'Unsupported URL' });
+      } else {
+        return res.status(400).json({ error: 'Unsupported URL' });
+      }
+  
       const videoTmp = tmp.fileSync({ postfix: '.mp4' });
 
       const response = await axios({
@@ -175,7 +213,7 @@ app.post('/api/download', async (req, res) => {
         method: 'GET',
         responseType: 'stream'
       });
-
+  
       response.data.pipe(fs.createWriteStream(videoTmp.name))
         .on('finish', () => {
           console.log('Video downloaded successfully');
@@ -193,7 +231,6 @@ app.post('/api/download', async (req, res) => {
           res.status(500).json({ error: 'Error downloading video' });
           videoTmp.removeCallback();
         });
-
     } catch (error) {
       console.error('Error:', error.message);
       res.status(500).json({ error: 'Error downloading video' });
